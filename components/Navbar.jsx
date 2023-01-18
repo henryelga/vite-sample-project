e983acf8a9034608e6c033e4580e7a78f436ef0a
Elga J Henry <henryelga2004@gmail.com>
2023-01-18 21:51:23 +0000

    +

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Navbar Component</h1>
+    <nav className="navbar">
+      <div className="logo">
+        <img src="./react.png" alt="" className="nav-logo" />
+        <h3>ReactFacts</h3>
+      </div>
+      <div>
+        <h4>React Course - Project 1</h4>
+      </div>
     </nav>
   );
 }
